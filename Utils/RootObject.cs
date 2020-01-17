de2a0934cffa08431393f7e21e4428b37165d244
ShaikMohideen92 <shaik.syedabdulkader@avanade.com>
2020-01-17 17:21:30 +0530

    Slack Enablement

--- Utils/RootObject.cs
+++ Utils/RootObject.cs
@@ -11,6 +11,7 @@ namespace QnABot.Utils
         public string SubTitle { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public string SlackData { get; set; }
         public string VideoUrl { get; set; }
         public string CtaLink { get; set; }
     }
